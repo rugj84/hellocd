bf632c3a084009cbbc285edf83442b72c9647144
rugj84 <124627036+rugj84@users.noreply.github.com>
2025-02-20 07:38:55 +0000

    fix: Update greeting to Hello Continuous Delivery!

--- src/main/java/ITJbootcamp/hellocd/HellocdApplication.java
+++ src/main/java/ITJbootcamp/hellocd/HellocdApplication.java
@@ -15,7 +15,7 @@ public class HellocdApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!";
+		return "Hello Continuous Delivery!";
 	}
 
 	public static void main(String[] args) {
